05d0bd4552b83dd3669860d5dcc4dcad5e27276e
BogdanBozga <bogdan.bozga01@gmail.com>
2024-01-30 07:44:07 +0200

    Updates

--- MainQLGW.py
+++ MainQLGW.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from QLearnFL import AgentQFL
 from DQLearnFL import AgentDQFL
-
+from NQLeanFL import AgentNQFL
 
 def run_agent(agent, env, n_games):
     scores = []
@@ -39,34 +39,10 @@ def run_agent(agent, env, n_games):
 
 if __name__ == "__main__":
     env = GridWorld()
+    nr_qs = 10
     agentQ = AgentQFL(lr=0.1, gamma=0.95, eps_start=1.0, eps_end=0.01, eps_dec=0.999999, n_actions=4, n_states=8*8)
     agentDQ = AgentDQFL(lr=0.1, gamma=0.95, eps_start=1.0, eps_end=0.01, eps_dec=0.999999, n_actions=4, n_states=8*8)
-
+    agentNQ = AgentNQFL(lr=0.1, gamma=0.95, eps_start=1.0, eps_end=0.01, eps_dec=0.999999, n_actions=4, n_states=8*8,nq=nr_qs)
 
     run_agent(agentQ, env, 1000000)
     run_agent(agentDQ, env, 1000000)
-
-
-    # scores = []
-    # win_pcts = []
-    # nr_steps_l = []
-    # for i in range(n_games):
-    #     done = False
-    #     state = env.reset()
-    #     score = 0
-    #     while not done:
-    #         action = agentDQ.choose_action(state)
-    #         new_state, reward, done, nr_steps = env.step(action)
-    #
-    #         agentDQ.learn(state, action, reward, new_state)
-    #         score += reward
-    #         state = new_state
-    #     scores.append(score)
-    #     scores.append(score)
-    #     if i % 100 == 0:
-    #         win_pct = np.mean(scores[-100:])
-    #         win_pcts.append(win_pct)
-    #         if i % 1000 == 0:
-    #             print('episode ', i, 'win pct %.2f' % win_pct, 'epsilon %.2f' % agentDQ.epsilon)
-    # plt.plot(win_pcts)
-    # plt.show()
